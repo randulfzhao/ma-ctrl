4397e1e2fe82653d018f9c8f2867d8eafedfecaf
randulfzhao <120090715@link.cuhk.edu.cn>
2026-02-14 11:40:47 -0500

    buffer 50, step 150

--- run_enode/cooperative_navigation_0.sh
+++ run_enode/cooperative_navigation_0.sh
@@ -21,7 +21,7 @@ for SEED in "${SEEDS[@]}"; do
     --consensus_weight 0.02 \
     --device "cuda:${GPU_ID}" \
     --episode_length 25 \
-    --replay_buffer_size 75 \
+    --replay_buffer_size 50 \
     --dyn_max_episodes 100 \
     --discount_rho 0.95 \
     --soft_update_tau 0.001 \
@@ -29,7 +29,7 @@ for SEED in "${SEEDS[@]}"; do
     --critic_lr 0.001 \
     --dyn_lr 0.001 \
     --dyn_grad_clip 3.0 \
-    --dyn_batch_size 128 \
+    --dyn_batch_size 256 \
     --dyn_update_steps 150 \
     --dyn_window_steps 8 \
     --rew_lr 0.001 \
@@ -48,7 +48,7 @@ for SEED in "${SEEDS[@]}"; do
     --wandb_group cooperative-navigation-enode \
     --wandb_name "cooperative-navigation-enode-gpu${GPU_ID}-seed${SEED}-${RUN_TS}" \
     --wandb_mode online \
-    --wandb_tags "enode,cooperative_navigation,dt0.1,dynlr3e-4,dynclip3,dynbs128,dynsteps150,dynwin8,rb75,dynmax100,neweps4,nens5" \
+    --wandb_tags "enode,cooperative_navigation,dt0.1,dynlr1e-3,dynclip3,dynbs256,dynsteps150,dynwin8,rb50,dynmax100,neweps4,nens5" \
     --wandb_entity "" \
     2>&1 | tee "logs/cooperative_navigation_enode_gpu${GPU_ID}_seed${SEED}_${RUN_TS}.log"
 done
